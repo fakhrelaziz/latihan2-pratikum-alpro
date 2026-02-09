d83f3ecdf9c3103f67ea67c4c287d7e060a3b55a
Fakhrel Aziz Maarif <fakhrelazizmaarif@gmail.com>
2026-02-09 23:51:17 +0700

    initial commit

---
+++ latihan2.py
@@ -0,0 +1,16 @@
+def is_password_valid(password):
+    if len(password) < 8:
+        return False
+    has_upper = False
+    has_digit = False
+    has_space = False
+    for char in password:
+        if char.isupper():
+            has_upper = True
+        if char.isdigit():
+            has_digit = True
+        if char.isspace():
+            has_space = True
+    return has_upper and has_digit and not has_space
+     
+          
